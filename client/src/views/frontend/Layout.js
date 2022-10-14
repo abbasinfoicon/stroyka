51dcb2fd984a55bfa5eb68c4720a520963e1903c
abbasinfoicon <uid3@infoiconsoftware.com>
2022-10-14 19:03:35 +0530

    push

--- client/src/views/frontend/Layout.js
+++ client/src/views/frontend/Layout.js
@@ -2,12 +2,15 @@ import React from 'react'
 import Footer from '../../components/lib/Footer'
 import Header from '../../components/lib/Header'
 import { Outlet } from 'react-router-dom';
+import Index from '../../routes/Index';
 
 const Layout = () => {
     return (
         <>
             <Header />
-            <Outlet />
+            <div className='main'>
+                <Index/>
+            </div>
             <Footer />
         </>
     )
